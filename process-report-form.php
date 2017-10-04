77e5d0b0c99dfba1beb119c15b78a56cfd15430f
Kevin <kevinangles@outlook.com>
2017-10-04 17:03:53 -0400

    Renamed files for consistency

--- process-report-form.php
+++ process-report-form.php
@@ -1,36 +1,34 @@
 <?php
-require_once 'cn.php';
-require_once 'protect.php';
+	require_once 'cn.php';
+	require_once 'protect.php';
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-// $student_id = $_POST['student_id'];
-$email = $_POST['email'];
-$item_name = $_POST['item_name'];
-$item_description = $_POST['item_description'];
-$date_reported = time();
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$item_name = $_POST['item_name'];
+	$item_description = $_POST['item_description'];
+	$date_reported = time();
 
-if (empty($first_name) || empty($last_name) || empty($email) || empty($item_name) || empty($item_description)){
-	header("Location: ../Panther Finder/index.php?error=empty");
-	exit();
-} else {
-	$sql = 'INSERT INTO reports
-	(first_name,
-	last_name,
-	email,
-	item_name,
-	item_description,
-  date_reported)
-		VALUES
-	("' . $first_name . '",
-	 "' . $last_name . '",
-	 "' . $email . '",
-	 "' . $item_name . '",
-	 "' . $item_description . '",
-	 "' . $date_reported . '")';
+	if (empty($first_name) || empty($last_name) || empty($email) || empty($item_name) || empty($item_description)){
+		header("Location: ../Panther Finder/index.php?error=empty");
+		exit();
+	} else {
+		$sql = 'INSERT INTO reports
+		(first_name,
+		last_name,
+		email,
+		item_name,
+		item_description,
+	  date_reported)
+			VALUES
+		("' . $first_name . '",
+		 "' . $last_name . '",
+		 "' . $email . '",
+		 "' . $item_name . '",
+		 "' . $item_description . '",
+		 "' . $date_reported . '")';
 
-$result = mysqli_query($cn, $sql) or
-	die(mysqli_error($cn));
-	header("Location: /");
-}
+		$result = mysqli_query($cn, $sql) or die(mysqli_error($cn));
+		header("Location: /");
+	}
 ?>
